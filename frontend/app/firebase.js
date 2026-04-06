9f86fd6d6dc3a05f2a35b581b62d12b533d06be7
Monisha <monishasundaram17@email.com>
2026-04-06 12:06:06 +0530

    changes made

--- frontend/app/firebase.js
+++ frontend/app/firebase.js
@@ -2,9 +2,9 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
+  apiKey: "AIzaSyDlfddzDbH9KBWVsXFZWfChBwz2Lf8hy-M",
+  authDomain: "urbanpulse-8c8cb.firebaseapp.com",
+  projectId: "urbanpulse-8c8cb",
 };
 
 const app = initializeApp(firebaseConfig);
